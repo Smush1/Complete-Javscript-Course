d3288c6020dce64ec18a17f3b1268e01318c5eca
sumeshk1 <sumesh1712@gmail.com>
2025-11-02 22:17:55 +0530

    Menu Fade Animation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -12,6 +12,14 @@ const btnScrollTo = document.querySelector('.btn--scroll-to');
 
 const section1 = document.querySelector('#section--1');
 
+const nav = document.querySelector('.nav')
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -118,9 +126,7 @@ document.querySelector('.nav__links').addEventListener('click',(e)=>{
 
 //  Tabbed Components
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container')
-const tabsContent = document.querySelectorAll('.operations__content')
+
 
 // tabs.forEach(t=>t.addEventListener('click',(e)=>{
 //   console.log('TAB')
@@ -149,6 +155,51 @@ tabsContainer.addEventListener('click',(e)=>{
 })
 
 
+// Menu Fade Animation
+
+const handleHover = (e, opacity)=>{
+      if(e.target.classList.contains('nav__link')){
+      const link = e.target;
+      const siblings = e.target.closest('.nav').querySelectorAll('.nav__link');
+      const logo = e.target.closest('.nav').querySelector('img')
+
+      siblings.forEach((el)=>{
+        if( el !== link){ 
+          el.style.opacity = opacity;
+          logo.style.opacity = opacity;
+         }
+      })
+    }
+}
+
+
+// passing "argument" to event handler
+
+
+nav.addEventListener('mouseover', function(e){
+  handleHover(e, 0.5)
+}
+  
+  
+  /* (e)=>{
+    if(e.target.classList.contains('nav__link')){
+      const link = e.target;
+      const siblings = e.target.closest('.nav').querySelectorAll('.nav__link');
+      const logo = e.target.closest('.nav').querySelector('img')
+
+      siblings.forEach((el)=>{
+        if( el !== link){ 
+          el.style.opacity = 0.5;
+          logo.style.opacity = 0.5;
+         }
+      })
+    }
+} */)
+
+
+nav.addEventListener('mouseout', function(e){
+  handleHover(e, 1)
+})
 
 
 
